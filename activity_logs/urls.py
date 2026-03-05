83d423a1bdf756ccf73ec4ba3fec23866189286c
Rahul <rahuldas9208@gmail.com>
2026-03-05 16:39:47 +0530

    done

--- activity_logs/urls.py
+++ activity_logs/urls.py
@@ -5,4 +5,5 @@ app_name = 'activity_logs'
 
 urlpatterns = [
     path('system-logs/', SystemLogListView.as_view(), name='system_logs'),
+    path('system-report/', SystemReportView.as_view(), name='system_report'),
 ]
