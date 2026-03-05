578ccca55989b8b91dbf317557a83dff04ef3cc9
Rahul <rahuldas9208@gmail.com>
2026-03-05 16:26:39 +0530

    done

--- activity_logs/urls.py
+++ activity_logs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SystemLogListView
+from .views import SystemLogListView, SystemReportView
 
 app_name = 'activity_logs'
 
 urlpatterns = [
     path('system-logs/', SystemLogListView.as_view(), name='system_logs'),
+    path('system-report/', SystemReportView.as_view(), name='system_report'),
 ]
